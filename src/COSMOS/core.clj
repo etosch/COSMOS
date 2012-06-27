41d16d91156f03c3e74eda02f8629852a0bb13c9
Emma Tosch <etosch@gmail.com>
2012-06-27 14:34:07 -0400

    fixed bug in keys args. cleaned up a little

--- src/COSMOS/core.clj
+++ src/COSMOS/core.clj
@@ -21,19 +21,29 @@
   (and (> runs config/increment)
        (< (/ new-xbar xbar) (+ 1 config/epsilon))
        (> (/ new-xbar xbar) (- 1 config/epsilon))))
-  
+
+(defn recompute-mean [old-mean old-runs-count new-data-point]
+  (/ (+ (* old-mean old-runs-count) new-data-point) (inc old-runs-count)))
+
+(defn- return-data [recs converged unconverged]
+  (reset! last-computed (with-meta recs {:converged converged :unconverged unconverged})))
+
 ;; Note: this would be very nice to have implemented using pattern matching, but due to issues in the :seq testing of core.match, I've opted to use basic destructuring.
 (defn cosmos-1
-  "Takes a list of maps of keys :ord, :val, :gen as input. Returns a vector whose length is the number of generations and whose contents are maps of the required number of runs and the fitness/error. Information on converged and unconverged [gen ord] pairs contained in the metadata."
+  "Takes a list of maps of keys :ord, :val, :gen as input. Returns a vector whose length is the number of generations and whose contents are maps of the required number of runs and the fitness/error. Information on converged and unconverged [gen ord] pairs contained in the metadata.
+
+Default implementation assumes that data comes in a stream. This means that if all points seen thus far have converged, the algorithm will return. If the algorithm should actually process all data it sees before returning, please specify a second argument set to a value that evaluates to true."
   ([data-seq full-stream?]
      (loop [recs (sorted-map) data data-seq converged #{} unconverged #{}]
-       (cond (not (seq data)) (reset! last-computed (with-meta recs {:converged converged :unconverged unconverged}))
-	     (and (not full-stream?) (seq converged) (not (seq unconverged))) (reset! last-computed (with-meta recs {:converged converged :unconverged unconverged}))
+       (cond (not (seq data))
+	     (return-data recs converged unconverged)
+	     (and (not full-stream?) (seq converged) (not (seq unconverged)))
+	     (return-data recs converged unconverged)
 	     :else (let [[{ord :ord val :val gen :gen} & tail] data]
 		     (if (contains? converged [gen ord]) 
 		       (recur recs tail converged unconverged)
 		       (let [{runs :runs xbar :xbar} (-> recs (get gen {}) (get ord {:runs 0 :xbar 1}))
-			     new-xbar (/ (+ (* xbar runs) val) (inc runs))]
+			     new-xbar (recompute-mean xbar runs val)]
 			 (recur (assoc recs gen (assoc (get recs gen) ord {:runs (inc runs) :xbar new-xbar}))
 				tail
 				(if (converged? runs xbar new-xbar) (conj converged [gen ord]) converged)
@@ -45,12 +55,15 @@
   "A private helper function for returning the converged data when selecting the maximum over this set."
   [unconverged cd] ;; there should be fewer unconverged than converged
   (let [[gen ord] (first unconverged)]
-    (if (empty? unconverged) cd (recur (rest unconverged) (assoc cd gen (dissoc (get cd gen) ord))))))
+    (if (empty? unconverged)
+      cd
+      (recur (rest unconverged) (assoc cd gen (dissoc (get cd gen) ord))))))
 
 (defn recommended-runs
   "Returns the maximum number of recommended runs. Can specify whether we should return the max-value only for the converged runs."
-  ([cosmos-data & {:keys [converged-only] :or {:converged-only false}}]
-     (let [data (or (and (not converged-only) cosmos-data) (get-converged-data (seq (:unconverged (meta cosmos-data))) cosmos-data))]
+  ([cosmos-data & {:keys [converged-only] :or {converged-only false}}]
+     (let [data (or (and (not converged-only) cosmos-data)
+		    (get-converged-data (seq (:unconverged (meta cosmos-data))) cosmos-data))]
        (apply max (map :runs (mapcat vals (vals data))))))
   ([] (recommended-runs @last-computed)))
 
@@ -66,7 +79,7 @@
 
 (defn recommended-runs-summary
   "Returns the highest generation containing the maximum number of recommended runs. If the highest generation with the maximum number of runs has not converged, can specify that we want the highest generation with the maxmum number of runs that has converged by using keyword :converged-only."
-  ([cosmos-data & {:keys [converged-only] :or {:converged-only false}}]
+  ([cosmos-data & {:keys [converged-only] :or {converged-only false}}]
      (let [data (or (and (not converged-only) cosmos-data) (get-converged-data (seq (:unconverged (meta cosmos-data))) cosmos-data))
 	   max-runs (recommended-runs data)]
        (loop [gens (reverse (sort (keys data)))]
